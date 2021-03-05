79cd99ebaacb5d3a63c21e49b429531a79d1eeaf
MatheusF99 <mc79846@gmail.com>
2021-03-05 08:48:42 -0300

    editando pagina de usuarios

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+
 import Sidebar from '../../components/sidebar/sidebar';
+import Headers from '../../components/header/header'
 
 // import { Container } from './styles';
+import './Home.css'
 
 const Home = () => {
   return (
-    <div className="">
+    <div className="homePage">
       <Sidebar />
-      <h1>Hello world</h1>
+
+      <div className="homeContent">
+
+        <Headers />
+
+        <div className="home">
+          <h1>Hello world</h1>
+        </div>
+
+      </div>
     </div>
   );
 }
--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,13 +1,119 @@
 import React from 'react';
+
 import Sidebar from '../../components/sidebar/sidebar';
+import Headers from '../../components/header/header'
+
+import './Users.css'
+
 
 // import { Container } from './styles';
 
 const Users = () => {
   return (
-    <div className="">
+    <div className="userPage">
       <Sidebar />
-      <h1>Hello world</h1>
+
+      <div className="userContent">
+
+        <Headers />
+
+        <div className="user">
+
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+              </tr>
+              <tr>
+                <th>Email</th>
+              </tr>
+              <tr className="type">
+                <th>Tipo</th>
+              </tr>
+              <tr>
+                <th>data de Cadastro</th>
+              </tr>
+              <tr>
+                <th>Opções</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>Matheus</td>
+              </tr>
+              <tr>
+                <td>matheus@email.com</td>
+              </tr>
+              <tr className="type">
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>12-02-1999</td>
+              </tr>
+              <tr>
+                <td>Excluir/alterar</td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td>Matheus</td>
+              </tr>
+              <tr>
+                <td>matheus@email.com</td>
+              </tr>
+              <tr className="type">
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>12-02-1999</td>
+              </tr>
+              <tr>
+                <td>Excluir/alterar</td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td>Matheus</td>
+              </tr>
+              <tr>
+                <td>matheus@email.com</td>
+              </tr>
+              <tr className="type">
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>12-02-1999</td>
+              </tr>
+              <tr>
+                <td>Excluir/alterar</td>
+              </tr>
+            </tbody>
+
+            <tbody>
+              <tr>
+                <td>Matheus</td>
+              </tr>
+              <tr>
+                <td>matheus@email.com</td>
+              </tr>
+              <tr className="type">
+                <td>1</td>
+              </tr>
+              <tr>
+                <td>12-02-1999</td>
+              </tr>
+              <tr>
+                <td>Excluir/alterar</td>
+              </tr>
+            </tbody>
+
+          </table>
+        </div>
+
+      </div>
     </div>
   );
 }
